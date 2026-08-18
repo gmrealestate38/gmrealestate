2d8d935bae304b2c964fb6d564098a6544ee45f6
gmrealestate38 <gmrealestate38@gmail.com>
2026-08-18 17:32:57 +0500

    Fix Gmail SMTP settings

--- gm_backend/settings.py
+++ gm_backend/settings.py
@@ -126,8 +126,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apna.email@gmail.com'        # <-- apna Gmail address daalein
-EMAIL_HOST_PASSWORD = 'xxxx xxxx xxxx xxxx'      # <-- 16-character App Password daalein
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # =========================================================
