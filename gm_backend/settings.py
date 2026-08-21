ff2af6d5f596f2bf21304b4e707544f3c0ed2095
gmrealestate38 <gmrealestate38@gmail.com>
2026-08-21 21:24:11 +0500

    Fix mobile API CSRF authentication

--- gm_backend/settings.py
+++ gm_backend/settings.py
@@ -243,8 +243,7 @@ CSRF_TRUSTED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
+        ],
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
